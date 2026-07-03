b6210163c5a1cea8eef5c9febd5447c19614133f
sanaa30-01 <s2taneja@ucsd.edu>
2026-07-03 13:15:48 +0530

    Added scope and number game folder and practice file within it

---
+++ scope_number_game/practice.py
@@ -0,0 +1,16 @@
+#practicing scope using prime number check
+
+def is_prime(num):
+    if num == 1:
+        return False
+    
+    if num == 2:
+        return True
+    
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    
+    return True 
+
+print(is_prime(73))
