144073412d370676954dc6be754057766e256890
sanaa30-01 <s2taneja@ucsd.edu>
2026-07-07 14:15:15 +0530

    Added three more files to debugging folder

---
+++ Debugging/fix_errors.py
@@ -0,0 +1,10 @@
+try:
+    age = int(input("How old are you?"))
+except ValueError:
+    print("You have typed in an invalid age.")
+    print("Please try again with a numerical value like 25.")
+    age = int(input("How old are you?"))
+
+if age > 18:
+    print(f"You can drive at age {age}.")
+
---
+++ Debugging/play_computer.py
@@ -0,0 +1,7 @@
+year = int(input("What's your year of birth?"))
+
+if year > 1980 and year < 1994:
+    print("You are a millenial.")
+elif year >= 1994: 
+    print("You are a Gen Z.")
+
